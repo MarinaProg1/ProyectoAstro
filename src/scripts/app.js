ee6edf6f15aa6e2d2d11b26978a074cae34379d1
MarinaProg1 <153437784+MarinaProg1@users.noreply.github.com>
2025-12-21 17:56:16 -0300

    Corregí mensaje de exito en el from y las imagen del avatar

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,19 +1,30 @@
-const elementos = document.querySelectorAll(".aparece");
-const form = document.getElementById("contact-form");
-const mensaje = document.getElementById("mensaje-enviado");
+document.addEventListener("DOMContentLoaded", () => {
+
+  const elementos = document.querySelectorAll(".aparece");
+  const form = document.getElementById("contact-form");
+  const mensaje = document.getElementById("mensaje-enviado");
 
   const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
-        entry.target.classList.add("visible"); 
+        entry.target.classList.add("visible");
       }
     });
-  }, {
-    threshold: 0.4 // se activa cuando el 40% del elemento es visible
-  });
+  }, { threshold: 0.4 });
 
   elementos.forEach(el => observer.observe(el));
 
-  form.addEventListener("submit", () => {
-  mensaje.style.display = "block";
+  form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  fetch(form.action, {
+    method: "POST",
+    body: new FormData(form),
+    mode: "no-cors", // 👈 CLAVE
+  })
+  .then(() => {
+    mensaje.classList.remove("oculto");
+    form.reset();
+  });
+});
 });
